8f2a75c1e9c74297f5aff87afaf619f2cd066029
Alojandro <aloserra92@gmail.com>
2023-09-13 13:23:04 +0200

    proyecto citas

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,43 @@
+import Paciente from "./Paciente";
+
+const ListadoPacientes = ({ pacientes, setPaciente, eliminarPaciente }) => {
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+    
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center mb10">
+            Listado Pacientes
+          </h2>
+
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className=" text-green-600 font-bold">Pacientes y citas</span>
+          </p>
+
+          {pacientes.map((paciente) => (
+            <Paciente
+              key={paciente.id}
+              paciente={paciente}
+              setPaciente={setPaciente}
+              eliminarPaciente={eliminarPaciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center mb10">
+            No hay pacientes
+          </h2>
+
+          <p className="text-xl mt-5 mb-10 text-center">
+            Agrega tus pacientes {""}
+            <span className=" text-green-600 font-bold">,aparecerán aqui</span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListadoPacientes;
